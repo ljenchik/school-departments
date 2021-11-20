76823c56a2a837aa9544b264c6211707e0edd29f
aaxddppp <aaxddppp@email.com>
2021-11-20 23:47:08 +0000

    added date validation and formatting

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+
 from flask import Flask, render_template, request, redirect
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
+from dateutil.relativedelta import relativedelta
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:1234@localhost/dep'
@@ -81,30 +83,61 @@ def edit(id):
         return render_template('department.html', dep=dep_to_edit)
 
 
-@app.route('/department/<int:department_id>/employees', methods=['POST', 'GET'])
+@app.route('/department/<int:department_id>/employees')
 def index_emp(department_id):
+    employees = Employee.query.filter_by(department_id=department_id).order_by(Employee.date_created).all()
+    dep = Department.query.get_or_404(department_id)
+    return render_template('employees.html', employees=employees, department_name=dep.name,
+                           department_id=department_id)
+
+
+def validate_employee(emp: Employee) -> str:
+    if relativedelta(emp.start_date, emp.date_of_birth).years < 18:
+        return 'Employee must be at least 18 to start work'
+    if datetime.today().year - emp.date_of_birth.year > 100:
+        return "Please check employee's date of birth"
+
+
+def parse_float(value):
+    return float((value).replace(" ", "").replace(",", ""))
+
+
+@app.route('/add-employee/<int:department_id>', methods=['POST', 'GET'])
+def employee_add(department_id):
     if request.method == 'POST':
-        employee_name = request.form['name']
-        new_employee = Employee(name=employee_name, department_id=department_id)
+        dep = Department.query.get_or_404(department_id)
+        new_employee = Employee(department_id=department_id)
+        fill_employee_from_request(new_employee)
+        validation_error = validate_employee(new_employee)
+
+        if validation_error is not None:
+            return render_template('employee.html', employee=new_employee, department_name=dep.name,
+                                   department_id=department_id,
+                                   error=validation_error)
+
         try:
             db.session.add(new_employee)
             db.session.commit()
             link = f'/department/{department_id}/employees'
             return redirect(link)
-        except:
-            return 'There was an issue adding a new department'
+        except Exception as e:
+            print('There was an issue adding a new employee: ' + str(e))
+            db.session.rollback()
+            return render_template('employee.html', employee=new_employee, department_name=dep.name,
+                                   department_id=department_id,
+                                   error='There was an issue adding a new employee: ' + str(e))
     else:
-        employees = Employee.query.filter_by(department_id=department_id).order_by(Employee.date_created).all()
         dep = Department.query.get_or_404(department_id)
-        return render_template('employees.html', employees=employees, department_name=dep.name,
-                               department_id=department_id)
+        employee = Employee(name='', role='', salary='', start_date=datetime.today())
+        return render_template('employee.html', employee=employee, department_name=dep.name, department_id=dep.id, error='')
 
 
-@app.route('/add-employee/<int:id>')
-def employee_add(id):
-    dep = Department.query.get_or_404(id)
-    return render_template('employee.html', employee=Employee(name='', role='', date_of_birth='', salary='', start_date=''),
-                           department_name=dep.name, department_id = dep.id)
+def fill_employee_from_request(new_employee):
+    new_employee.name = request.form['name']
+    new_employee.role = request.form['role']
+    new_employee.date_of_birth = datetime.strptime(request.form['date_of_birth'], '%Y-%m-%d')
+    new_employee.salary = parse_float(request.form['salary'])
+    new_employee.start_date = datetime.strptime(request.form['start_date'], '%Y-%m-%d')
 
 
 @app.route('/delete-employee/<int:employee_id>')
@@ -123,17 +156,28 @@ def emp_delete(employee_id):
 def emp_edit(employee_id):
     emp_to_edit = Employee.query.get_or_404(employee_id)
     dep = Department.query.get_or_404(emp_to_edit.department_id)
+
     if request.method == 'POST':
-        emp_to_edit.name = request.form['name']
+        fill_employee_from_request(emp_to_edit)
+        validation_error = validate_employee(emp_to_edit)
+
+        if validation_error is not None:
+            return render_template('employee.html', employee=emp_to_edit, department_name=dep.name,
+                                   department_id=dep.id, error=validation_error)
 
         try:
             db.session.commit()
             link = f'/department/{emp_to_edit.department_id}/employees'
             return redirect(link)
-        except:
-            return 'There is an issue with editing this employee'
+        except Exception as e:
+            print('There was an issue adding a new employee: ' + str(e))
+            db.session.rollback()
+            return render_template('employee.html', employee=emp_to_edit, department_name=dep.name,
+                                   department_id=dep.id,
+                                   error='There was an issue editing an employee: ' + str(e))
     else:
-        return render_template('employee.html', employee=emp_to_edit, department_name=dep.name, department_id = dep.id)
+        return render_template('employee.html', employee=emp_to_edit, department_name=dep.name, department_id=dep.id,
+                               error='')
 
 
 if __name__ == "__main__":
